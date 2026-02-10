6ae11cb07fc5e0af5a84a6ab2e1fa50ec252c241
alireza <alireza.jvh98@gmail.com>
2026-02-10 20:43:13 +0100

    feat: create shread module named it common

--- auth/src/errors/index.ts
+++
@@ -1,6 +0,0 @@
-export { AbstractError } from './base';
-export { RequestValidationError } from './request-validation';
-export { DatabaseConnectionError } from './database-connection';
-export { NotFoundError } from './not-found';
-export { BadRequestError } from './bad-request';
-export type * from './types';
--- auth/src/errors/types.ts
+++
@@ -1,7 +0,0 @@
-export interface ErrorItem {
-  message: string;
-  field?: string;
-}
-
-export type ReturnError = { errors: ErrorItem[] };
-
